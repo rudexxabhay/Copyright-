cb4b16480c683af49591b770f26538ae42303fec
ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ꜱɪɴɢʜ <ks8817112@gmail.com>
2024-07-19 22:48:23 +0530

    config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ API_ID = int(os.environ.get("API_ID"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN" , "7122527579:AAHNITKuTS8NxMTWwEuG9O1ymgKEBm3d9ew")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_USERNAME = os.environ.get("BOT_USERNAME" , "RudexCopyright_Bot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_ID = int(os.environ.get("OWNER_ID" , "7374966263"))
 # ------------------X------------------------------
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID" , "-1002094142057"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
+MONGO_URL = os.environ.get("MONGO_URL" , "mongodb+srv://JARVISV1:JARVIS85@annie.bfgvyxi.mongodb.net/?retryWrites=true&w=majority&appName=Annie")
 # ------------------------------------------------
