1a7eb3c0ce5e2f00be40b0bf585a62058b8a4b12
ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ꜱɪɴɢʜ <ks8817112@gmail.com>
2024-07-20 00:04:53 +0530

    __main__.py

--- COPYRIGHT2/__main__.py
+++ COPYRIGHT2/__main__.py
@@ -6,8 +6,10 @@ from COPYRIGHT2.modules import ALL_MODULES
 
 LOGGER_ID = -1002094142057
 
-loop = asyncio.get_event_loop()
-if loop.is_closed():
+# Handling the event loop properly
+try:
+    loop = asyncio.get_event_loop()
+    if loop.is_closed():
         raise RuntimeError
 except RuntimeError:
     loop = asyncio.new_event_loop()
@@ -19,9 +21,8 @@ async def daxxpapa_boot():
         importlib.import_module("COPYRIGHT2.modules." + all_module)
     print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
     await idle()
-    print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @iam_daxx")
-    await COPYRIGHT2.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [𝐌𝚁°᭄𝐃𝙰𝚇𝚇 ࿐™ ](https://t.me/iam_daxx)**")
+    print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋")
+    await COPYRIGHT2.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝗋𝗎𝗇𝗇𝗂𝗇𝗀**")
 
 if __name__ == "__main__":
     loop.run_until_complete(daxxpapa_boot())
-    
